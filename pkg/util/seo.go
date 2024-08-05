791054af1652d7c616a70dbe1537ad74340e2ad0
dengmengmian(麻凡) <my@dengmengmian.com>
2024-08-05 13:04:47 +0800

    feat：增加前端加包命令

--- pkg/util/seo.go
+++ pkg/util/seo.go
@@ -1,3 +1,8 @@
+// Copyright 2023 Innkeeper gotribe <info@gotribe.cn>. All rights reserved.
+// Use of this source code is governed by a Apache style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://www.gotribe.cn
+
 package util
 
 import (

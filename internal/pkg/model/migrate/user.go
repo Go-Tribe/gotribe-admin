dd46c5644f97a12ceed01e5aca40dddb09631351
麻凡 <my@dengmengmian.com>
2024-12-09 23:39:50 +0800

    feat:用户名新增头像以及生日字段

--- internal/pkg/model/migrate/user.go
+++ internal/pkg/model/migrate/user.go
@@ -6,19 +6,58 @@
 package migrate
 
 import (
+	"fmt"
 	"gorm.io/gorm"
 	"gotribe-admin/internal/pkg/model"
+	"log"
 )
 
 // 自动迁移表结构
-func userMigrate(db *gorm.DB) {
-	db.AutoMigrate(
-		&model.User{},
-	)
-	// 添加字段
-	//db.Migrator().AddColumn(&model.User{}, "Test")
+func userMigrate(db *gorm.DB) error {
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	// 自动迁移表结构
+	if err := tx.AutoMigrate(&model.User{}).Error; err != nil {
+		tx.Rollback()
+		return fmt.Errorf("auto migrate failed: %w", err)
+	}
+
+	// 添加生日字段
+	if err := tx.Migrator().AddColumn(&model.User{}, "birthday").Error; err != nil {
+		tx.Rollback()
+		return fmt.Errorf("add column birthday failed: %w", err)
+	}
+	// 增加用户头像
+	if err := tx.Migrator().AddColumn(&model.User{}, "avatar_url").Error; err != nil {
+		tx.Rollback()
+		return fmt.Errorf("add column avatar_url failed: %w", err)
+	}
+
 	// 删除字段
-	//db.Migrator().DropColumn(&model.User{}, "Test")
-	// 添加索引
-	//db.Migrator().CreateIndex(&model.User{}, "idx_name")
+	// if err := tx.Migrator().DropColumn(&model.User{}, "Test").Error; err != nil {
+	//     tx.Rollback()
+	//     return fmt.Errorf("drop column Test failed: %w", err)
+	// }
+
+	//手机号&邮箱唯一索引
+	if err := tx.Migrator().CreateIndex(&model.User{}, "idx_phone").Error; err != nil {
+		tx.Rollback()
+		return fmt.Errorf("create index idx_phone failed: %w", err)
+	}
+	if err := tx.Migrator().CreateIndex(&model.User{}, "idx_email").Error; err != nil {
+		tx.Rollback()
+		return fmt.Errorf("create index idx_phone failed: %w", err)
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		return fmt.Errorf("commit transaction failed: %w", err)
+	}
+
+	log.Println("User migration completed successfully")
+	return nil
 }

10ca09c7724651342efb937e098470e3480f12ba
麻凡 <my@dengmengmian.com>
2024-12-23 22:00:04 +0800

    feat:新增商品管理

--- pkg/api/vo/product_request.go
+++ pkg/api/vo/product_request.go
@@ -7,10 +7,17 @@ package vo
 
 // 创建产品结构体
 type CreateProductRequest struct {
-	Title      string `form:"title" json:"title" validate:"required,min=2,max=20"`
-	CategoryID string `form:"categoryID" json:"categoryID" validate:"required"`
-	SpecIds    string `form:"specIDs" json:"specIDs" validate:"required"`
-	Remark     string `form:"remark" json:"remark"`
+	Title         string `form:"title" json:"title" validate:"required,min=2,max=20"`
+	CategoryID    string `form:"categoryID" json:"categoryID" validate:"required"`
+	ProductNumber string `form:"productNumber" json:"productNumber"`
+	ProjectID     string `form:"projectID" json:"projectID"  validate:"required"`
+	Description   string `form:"description" json:"description"`
+	Image         string `form:"image" json:"image"`
+	Video         string `form:"video" json:"video"`
+	BuyLimit      uint   `form:"buyLimit" json:"buyLimit" validate:"required,min=1,max=20"`
+	ProductSpec   string `form:"productSpec" json:"productSpec"`
+	Content       string `form:"content" json:"content"`
+	Enable        uint   `form:"enable" json:"enable" validate:"oneof=1 2"`
 }
 
 // 获取产品列表结构体

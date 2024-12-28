0c599cc0582264f3430da663d16597967a77e930
麻凡 <my@dengmengmian.com>
2024-12-28 20:58:34 +0800

    feat:新增订单管理

--- pkg/api/vo/product_request.go
+++ pkg/api/vo/product_request.go
@@ -40,10 +40,10 @@ type Sku struct {
 	SKUID         string  `form:"skuID" json:"skuID"`
 	Title         string  `form:"skuTitle" json:"skuTitle" validate:"required"`
 	Image         string  `form:"image" json:"image" validate:"required"`
-	CostPrice     float64 `json:"cost_price" validate:"required"`
-	MarketPrice   float64 `json:"market_price" validate:"required"`
-	UnitPrice     float64 `json:"unit_price" validate:"required"`
-	UnitPoint     int     `json:"unit_point" validate:"required"`
+	CostPrice     float64 `json:"costPrice" validate:"required"`
+	MarketPrice   float64 `json:"marketPrice" validate:"required"`
+	UnitPrice     float64 `json:"unitPrice" validate:"required"`
+	UnitPoint     int     `json:"unitPoint" validate:"required"`
 	Quantity      int     `json:"quantity" validate:"required"`
-	EnableDefault int     `json:"enable_default"`
+	EnableDefault int     `json:"enableDefault"`
 }

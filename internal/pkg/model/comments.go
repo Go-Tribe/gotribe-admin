6a65d04d68c045b749923ee9e25672d4de1e50f5
麻凡 <my@dengmengmian.com>
2025-01-10 21:24:42 +0800

    feat:修改一些字段

--- internal/pkg/model/comments.go
+++ internal/pkg/model/comments.go
@@ -17,10 +17,14 @@ type Comment struct {
 	Type        uint   `gorm:"type:tinyint;not null;default:1;comment:评论类型，1-评论；2-回复" json:"type"`
 	UserID      string `gorm:"type:char(10);not null;index;comment:用户ID" json:"userID"`
 	ToUserID    string `gorm:"type:char(10);not null;index;comment:被评论用户ID" json:"toUserID"`
-	PID         int    `gorm:"type:int;not null;default:0;comment:父评论ID" json:"pid"`
+	ParentID    int    `gorm:"type:int;not null;default:0;comment:父评论ID" json:"parentID"`
 	ReplyToID   int    `gorm:"type:int;not null;default:0;comment:回复的评论ID" json:"ReplyToID"`
 	Hot         int    `gorm:"type:int;default:0;comment:热度" json:"hot"`
 	Like        int    `gorm:"type:int;default:0;comment:点赞数" json:"like"`
 	Dislike     int    `gorm:"type:int;default:0;comment:踩数" json:"dislike"`
+	IP          string `gorm:"type:varchar(255);not null;comment:IP地址" json:"ip"`
+	Country     string `gorm:"type:varchar(255);not null;comment:国家" json:"country"`
+	RegionName  string `gorm:"type:varchar(255);not null;comment:地区" json:"regionName"`
+	City        string `gorm:"type:varchar(255);not null;comment:城市" json:"city"`
 	User        *User  `gorm:"-" json:"user"`
 }

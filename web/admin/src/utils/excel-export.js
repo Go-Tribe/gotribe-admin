7fc798a1af1cc2ce8873b326fcd842fb3b7586af
麻凡 <my@dengmengmian.com>
2025-01-13 22:43:50 +0800

    feat:修改文章价格

--- web/admin/src/utils/excel-export.js
+++
@@ -1,8 +0,0 @@
-import * as xlsx from 'xlsx'
-
-export const exportData = (data, name) => {
-  const worksheet = xlsx.utils.aoa_to_sheet(data)
-  const workbook = xlsx.utils.book_new()
-  xlsx.utils.book_append_sheet(workbook, worksheet, '表格数据')
-  xlsx.writeFile(workbook, `${name || 'list'}.xlsx`)
-}
--- web/admin/src/utils/get-page-title.js
+++
@@ -1,9 +0,0 @@
-import store from '@/store'
-
-export default function getPageTitle(pageTitle) {
-  const title = store.getters.systemConfig.title || 'Vue Element Admin'
-  if (pageTitle) {
-    return `${pageTitle} - ${title}`
-  }
-  return `${title}`
-}
